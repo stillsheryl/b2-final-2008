b4e39e990c658d6aa09f742fcb863a8ebbe22138
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-11-09 09:25:51 -0700

    Add tests for relationships between doctor_patients and doctors/patients

--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -3,5 +3,7 @@ require 'rails_helper'
 RSpec.describe Doctor do
   describe 'Relationships' do
     it should{belong_to :doctors}
+    it should{have_many :doctor_patients}
+    it should{have_many(:patients).through(:doctor_patients)}
   end
 end
