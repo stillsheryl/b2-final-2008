4d4bb26f6fa58365b6f0c9ef8daed9246af8adaf
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-11-09 09:47:12 -0700

    Add db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#Hospitals
+@hospital = Hospital.create!(name: "Grey Sloan Memorial Hospital")
+
+#Doctors
+@meredith = @hopital.doctors.create!(name: "Meredith Grey", specialty: "General Surgery", university: "Harvard University")
+
+#Patients
+@patient_1 = @meredith.patients.create!(name: "Katie Bryce", age: 24)
+@patient_2 = @meredith.patients.create!(name: "Denny Duquette", age: 39)
+@patient_3 = @meredith.patients.create!(name: "Rebecca Pope", age: 32)
