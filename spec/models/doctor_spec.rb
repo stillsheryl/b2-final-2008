0eca6f324ccca3c3496b03b6ff7f810c22a238b2
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-11-09 09:43:35 -0700

    Add test for instance method to grab hospital name where doc works

--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -6,4 +6,18 @@ RSpec.describe Doctor do
     it should{have_many :doctor_patients}
     it should{have_many(:patients).through(:doctor_patients)}
   end
+
+  describe "instance methods" do
+    before :each do
+      @hospital = Hospital.create!(name: "Grey Sloan Memorial Hospital")
+      @meredith = @hospital.doctors.create!(name: "Meredith Grey", specialty: "General Surgery", university: "Harvard University")
+      @patient_1 = @meredith.patients.create!(name: "Katie Bryce", age: 24)
+      @patient_2 = @meredith.patients.create!(name: "Denny Duquette", age: 39)
+      @patient_3 = @meredith.patients.create!(name: "Rebecca Pope", age: 32)
+    end
+
+    it "#working_hospital" do
+      expect(@meredith.working_hospital).to eq(@hospital.name)
+    end
+  end
 end
