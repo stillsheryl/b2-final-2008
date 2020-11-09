bc8c420edc5dc31f20db5d2f05f51ed00bf6bc84
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-11-09 11:09:23 -0700

    Update seeds and add re-direct on method

--- app/controllers/doctor_patients_controller.rb
+++ app/controllers/doctor_patients_controller.rb
@@ -2,5 +2,6 @@ class DoctorPatientsController < ApplicationController
   def destroy
     doc_relationship = DoctorPatient.select("doctor_id = #{params[:id]}").where("patient_id = #{params[:patient_id]}").pluck(:id).first
     DoctorPatient.destroy(doc_relationship)
+    redirect_to "/doctors/#{params[:id]}"
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,15 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 #Hospitals
-@hospital = Hospital.create!(name: "Grey Sloan Memorial Hospital")
+hospital = Hospital.create!(name: "Grey Sloan Memorial Hospital")
 
 #Doctors
-@meredith = @hopital.doctors.create!(name: "Meredith Grey", specialty: "General Surgery", university: "Harvard University")
-@alex = @hospital.doctors.create!(name: "Alex Karev", specialty: "Pediatric Surgery", university: "Johns Hopkins University")
-@miranda = @hospital.doctors.create!(name: "Miranda Bailey", specialty: "General Surgery", university: "Stanford University")
-@derek = @hospital.doctors.create!(name: "Derek McDreamy Shepherd", specialty: "Attending Surgeon", university: "Stanford University")
+meredith = hospital.doctors.create!(name: "Meredith Grey", specialty: "General Surgery", university: "Harvard University")
+alex = hospital.doctors.create!(name: "Alex Karev", specialty: "Pediatric Surgery", university: "Johns Hopkins University")
+miranda = hospital.doctors.create!(name: "Miranda Bailey", specialty: "General Surgery", university: "Stanford University")
+derek = hospital.doctors.create!(name: "Derek McDreamy Shepherd", specialty: "Attending Surgeon", university: "Stanford University")
 
 #Patients
-@patient_1 = @meredith.patients.create!(name: "Katie Bryce", age: 24)
-@patient_2 = @meredith.patients.create!(name: "Denny Duquette", age: 39)
-@patient_3 = @meredith.patients.create!(name: "Rebecca Pope", age: 32)
+patient_1 = meredith.patients.create!(name: "Katie Bryce", age: 24)
+patient_2 = meredith.patients.create!(name: "Denny Duquette", age: 39)
+patient_3 = meredith.patients.create!(name: "Rebecca Pope", age: 32)
