7af3d73d72e32c127a1052ee6b34ee93fd854aa5
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-11-09 09:59:53 -0700

    Add test for instance method to count number of docs at hospital

--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -4,4 +4,18 @@ RSpec.describe Hospital do
   describe 'Relationships' do
     it should{have_many :doctors}
   end
+
+  describe "instance methods" do
+    before :each do
+      @hospital = Hospital.create!(name: "Grey Sloan Memorial Hospital")
+      @meredith = @hospital.doctors.create!(name: "Meredith Grey", specialty: "General Surgery", university: "Harvard University")
+      @alex = @hospital.doctors.create!(name: "Alex Karev", specialty: "Pediatric Surgery", university: "Johns Hopkins University")
+      @miranda = @hospital.doctors.create!(name: "Miranda Bailey", specialty: "General Surgery", university: "Stanford University")
+      @derek = @hospital.doctors.create!(name: "Derek McDreamy Shepherd", specialty: "Attending Surgeon", university: "Stanford University")
+    end
+
+    it "#doctor_count" do
+      expect(@hospital.doctor_count).to eq(4)
+    end
+  end
 end
